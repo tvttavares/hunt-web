cde92d9b3a9686b956e84462f25e559ff4729bab
Thiago Tavares <tvttavares@gmail.com>
2020-06-20 21:58:22 -0400

    Criando o Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
+import './styles.css';
+
+import Header from './components/Header';
+
+const App = () => (
     <div className="App">
-      <h1>Hello Rocketseat</h1>
+      <Header />
     </div>
-  );
-}
+);
 
 export default App;
